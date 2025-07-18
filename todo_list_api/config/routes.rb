e798f2c160c700bbf1d904f9dec253da5f2844ae
sjtroxel <sjtroxel@protonmail.com>
2025-07-18 13:01:24 -0500

    added auth code to backend

--- todo_list_api/config/routes.rb
+++ todo_list_api/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  post '/login', to: 'sessions#create'
   resources :todos
+  resources :users, only: [:create]
 end
--- todo_list_api/db/schema.rb
+++ todo_list_api/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_14_212754) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_18_175339) do
   create_table "todos", force: :cascade do |t|
     t.string "body"
     t.boolean "is_completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
