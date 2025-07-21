0b9a8843a6d1dcae4ed6f82a4ece9fdbf42c19ac
sjtroxel <sjtroxel@protonmail.com>
2025-07-21 12:54:05 -0500

    betablogs thru lecture 0721 but mine isn't showing the blogs

--- beta_blogs_noon-api/app/blueprints/blog_blueprint.rb
+++ beta_blogs_noon-api/app/blueprints/blog_blueprint.rb
@@ -3,9 +3,10 @@
 class BlogBlueprint < Blueprinter::Base
   identifier :id
 
-  fields :title, :content
+  # fields :title, :content
 
   view :normal do
+    fields :title, :content
     association :user, blueprint: UserBlueprint, view: :normal
   end
 end
